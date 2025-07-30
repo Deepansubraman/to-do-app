9262e30a89783b0d21671ce8efcda858e4252500
927622BEC028 <144405213+Deepansubraman@users.noreply.github.com>
2025-07-30 11:50:21 +0530

    Update App.jsx

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -8,7 +8,7 @@ import { Tooltip } from 'react-tooltip';
 import 'react-tooltip/dist/react-tooltip.css';
 import Confetti from 'react-confetti';
 
-const API = 'https://to-do-app-3utk.onrender.com/api';
+const API = 'https://to-do-app-5-7owd.onrender.com/api';
 
 const priorityColors = {
   Low: '#4ade80',
